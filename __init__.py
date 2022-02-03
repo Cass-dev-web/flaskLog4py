19ff22304d1ba643b4d0e5c729a5a94e16d2ea5c
Cass <60286224+ParentProfanities@users.noreply.github.com>
2022-02-04 07:04:59 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ from tkinter import *
 from flask import Flask
 from subprocess import Popen
 from datetime import datetime
+import logging
 from enum import Enum
 #############
 #vars 
@@ -19,39 +20,35 @@ consoles = []
 startupHTML="""
 <!DOCTYPE html>
 <html>
-<head>
-<style>
-.container {
-width: 90em;
-overflow-x: auto;
-whte-space: nowrap;
-height: 50em;
-}
-textarea{
-height: 500px;
-width: 95%;
-}
-p {
-font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-font-size: 20px;
-}
-</style>
-</head>
-<body>
-<div class="container"><table><thead>THREQAD_CONTAIN</thead></table></div>
-</body>
-</html>
+    <head>
+        <style>
+            .container {width: 90em;overflow-x: auto;white-space: nowrap;height: 50em;} textarea{height: 500px;width: 95%;} p {font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;font-size: 20px;}
+        </style>
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script>
+        
+            function timeout(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+            var url = "consoles";
+            async function main(){
+                while(true){
+                    $.get(url, function (data) { 
+                        document.querySelector("body").innerHTML= data
+                    }); 
+                    
+                    await timeout(1000)
+                }
+            }
+            main()
+        </script>
+    </head>
+    <body>
+        Loading.. (Make sure javascript is enabled.)
+    </body></html>
 """
 templateDiv = '''
-<div>
-  <p style="text-align:left;">[dt]
-    <span style="float:right; padding-right: 10px; color:[cl]">
-        [st]
-    </span>
-  </p>
-  <hr>
-  <textarea readonly>[da]</textarea>
-</div><br>
+<div><p style="text-align:left;">[dt]<span style="float:right; padding-right: 10px; color:[cl]">[st]</span></p><hr><textarea readonly>[da]</textarea></div><br>
 '''
 ########## fun
 def newConsole(threadName):
@@ -63,16 +60,23 @@ def addLine(consoleNum, line, type):
 def changeStatus(consoleNum,text,color):
   consoles[consoleNum][2][0]=text
   consoles[consoleNum][2][1]=color
-##########################################################################################################################################################################
+##########################################################################################################################################################################\
+def calculateConsolesHTML():
+    consoles_parsed =""
+    for consoleElement in consoles:
+        consoleline = ""
+        for line in consoleElement[1]:
+            consoleline=consoleline+line+"\n"
+        consoles_parsed=consoles_parsed+(templateDiv.replace("[dt]",consoleElement[0]).replace("[da]",consoleline).replace("[st]",consoleElement[2][0]).replace("[cl]",consoleElement[2][1]))
+    return consoles_parsed
 def startServer(port):
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
     app = Flask(__name__)
     @app.route('/')
     def home():
-        consoles_parsed =""
-        for consoleElement in consoles:
-            consoleline = ""
-            for line in consoleElement[1]:
-                consoleline=consoleline+line+"\n"
-            consoles_parsed=consoles_parsed+(templateDiv.replace("[dt]",consoleElement[0]).replace("[da]",consoleline).replace("[st]",consoleElement[2][0]).replace("[cl]",consoleElement[2][1]))
-        return startupHTML.replace("THREQAD_CONTAIN",consoles_parsed)
-    app.run(port=port)
+        return startupHTML
+    @app.route('/consoles')
+    def getConsoles():
+        return '<div class="container"><table><thead>'+ calculateConsolesHTML()+'</thead></table></div>'
+    app.run(port=port, debug=False)
